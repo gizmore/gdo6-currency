39da27d7ac99e68931c605b40138324013375b39
gizmore <gizmore@wechall.net>
2017-08-24 21:07:34 +0200

    Initial commit.

---
+++ Currency.php
@@ -0,0 +1,71 @@
+<?php
+namespace GDO\Currency;
+
+use GDO\DB\GDO;
+use GDO\DB\GDO_UpdatedAt;
+use GDO\Type\GDO_Char;
+use GDO\Type\GDO_Checkbox;
+use GDO\Type\GDO_Decimal;
+use GDO\Type\GDO_Int;
+use GDO\Type\GDO_String;
+/**
+ * @author gizmore
+*/
+final class Currency extends GDO
+{
+// 	public function gdoCached() { return false; }
+	
+	###########
+	### GDO ###
+	###########
+	public function gdoColumns()
+	{
+		return array(
+			GDO_Char::make('ccy_iso')->ascii()->caseS()->size(3)->primary(),
+			GDO_String::make('ccy_symbol')->max(3)->notNull(),
+			GDO_Int::make('ccy_digits')->bytes(1)->unsigned()->min(1)->max(4),
+			GDO_Decimal::make('ccy_ratio')->digits(6, 6),
+			GDO_Checkbox::make('ccy_auto_update')->initial('1'),
+			GDO_UpdatedAt::make('ccy_updated_at'),
+		);
+	}
+
+	##############
+	### Getter ###
+	##############
+	public function getSymbol() { return $this->getVar('ccy_symbol'); }
+	public function isSyncAutomated() { return $this->getVar('ccy_auto_update') === '1'; }
+
+	################
+	### Display ####
+	################
+	public function displayValue($value, $with_symbol=true) { return sprintf('%s%.02f'.$this->getVar('curr_digits').'f', $with_symbol ? $this->getSymbol().'' : '', $value); }
+
+	###############
+	### Factory ###
+	###############
+// 	/**
+// 	* Return all available ISOs.
+// 	*/
+// 	public static function getISOs()
+// 	{
+// 		return self::table()->selectColumn('curr_iso');
+// 	}
+
+	/**
+	 * @param string $iso
+	 * @return self
+	 */
+	public static function getByISO($iso)
+	{
+		return self::getById($iso);
+	}
+	
+	##################
+	### Conversion ###
+	##################
+	public static function convert($value, $from, $to)
+	{
+
+	}
+}
---
+++ Module_Currency.php
@@ -0,0 +1,41 @@
+<?php
+namespace GDO\Currency;
+
+use GDO\Core\Module;
+use GDO\Date\GDO_Duration;
+use GDO\Date\GDO_Timestamp;
+use GDO\Date\Time;
+use GDO\Type\GDO_String;
+/**
+ * Builds a list of currency and conversion rates.
+ * Updates them via cronnjob.
+ * 
+ * @author gizmore
+ * @version 5.00
+ */
+final class Module_Currency extends Module
+{
+	##############
+	### Module ###
+	##############
+	public $module_priority = 10;
+	public function onLoadLanguage() { return $this->loadLanguage('lang/currency'); }
+	public function getClasses() { return array('GDO\Currency\Currency'); }
+
+	##############
+	### Config ###
+	##############
+	public function getConfig()
+	{
+		return array(
+			GDO_Timestamp::make('ccy_last_try')->initial(0),
+			GDO_String::make('ccy_last_sync'),
+			GDO_Duration::make('ccy_update_fqcy')->initial(Time::ONE_HOUR),
+		);
+	}
+	public function cfgUpdateEnabled() { return $this->cfgUpdateFrequency() > 0; }
+	public function cfgLastTry() { return $this->getConfigValue('ccy_last_try'); }
+	public function cfgLastSync() { return $this->getConfigValue('ccy_last_sync'); }
+	public function cfgUpdateFrequency() { return $this->getConfigValue('ccy_update_fqcy'); }
+	
+}
