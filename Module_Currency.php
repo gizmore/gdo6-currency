897d9f922073bbc2394c42b6f1684aaeaa0181ab
gizmore <gizmore@wechall.net>
2021-11-12 23:18:02 +0100

    6.10.6-r6175_cronjob

--- Module_Currency.php
+++ Module_Currency.php
@@ -4,14 +4,15 @@ namespace GDO\Currency;
 use GDO\Core\GDO_Module;
 use GDO\Date\GDT_Duration;
 use GDO\Date\GDT_Timestamp;
-use GDO\Date\Time;
 use GDO\DB\GDT_String;
+
 /**
  * Builds a list of currency and conversion rates.
  * Updates them via cronnjob.
  * 
  * @author gizmore
- * @version 5.00
+ * @version 6.10.6
+ * @since 5.0.0
  */
 final class Module_Currency extends GDO_Module
 {
@@ -20,18 +21,19 @@ final class Module_Currency extends GDO_Module
 	##############
 	public $module_priority = 10;
 	public function onLoadLanguage() { return $this->loadLanguage('lang/currency'); }
-	public function getClasses() { return array('GDO\Currency\GDO_Currency'); }
+	public function getClasses() { return [GDO_Currency::class]; }
+	public function getDependencies() { return ['Cronjob']; }
 
 	##############
 	### Config ###
 	##############
 	public function getConfig()
 	{
-		return array(
+		return [
 			GDT_Timestamp::make('ccy_last_try')->initial(0),
 			GDT_String::make('ccy_last_sync'),
 			GDT_Duration::make('ccy_update_fqcy')->initial("1h"),
-		);
+		];
 	}
 	public function cfgUpdateEnabled() { return $this->cfgUpdateFrequency() > 0; }
 	public function cfgLastTry() { return $this->getConfigValue('ccy_last_try'); }
